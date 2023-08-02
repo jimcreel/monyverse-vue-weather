0cb3af4f3c6841bfcec93a15292905ed1d3fadf3
jimcreel <jim.creel@gmail.com>
2023-08-01 23:20:18 -0700

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import TodayView from '../views/TodayView.vue'
 import FiveDayView from '../views/Forecast/FiveDayView.vue'
 
-const routes = [
+const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
