2335183a2dce419ea70afa9e78d23378b8f84ff8
jimcreel <jim.creel@gmail.com>
2023-08-02 01:01:18 -0700

    reverting to requires for server

--- server.ts
+++ server.ts
@@ -1,18 +1,19 @@
-import express, { Request, Response } from 'express';
-import serveStatic from 'serve-static';
-import path from 'path';
 
-const app = express();
 
-// Here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-// This * route is to serve project on different page routes except root `/`
-app.get(/.*/, (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
-});
+const app = express()
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`app is listening on port: ${port}`);
-});
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
